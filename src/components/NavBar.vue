2a88eb97e166b5be7c8fda54b5b65547db1da31e
Wilson Onjore <wilson.onjore@strathmore.edu>
2023-10-14 22:12:42 +0300

    Changemenu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,13 @@
 <template>
   <div>
     <v-toolbar>
-      <!--Hamburger icon-->
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-avatar size="40">
+      <v-avatar size="30">
         <v-img src="/images/logo-2-itn.png"></v-img>
       </v-avatar>
       <v-toolbar-title>ITNomads</v-toolbar-title>
       <v-spacer></v-spacer>
-      <!---Menu tab-->
-      <v-btn prepend-icon="mdi-arrow-down-drop-circle" color="grey">
-        Menu
-        <v-menu activator="parent" open-on-hover>
-          <v-list class="text-purple-lighten-2">
-            <v-list-item
-              v-for="link in links"
-              :key="link.text"
-              router
-              :to="link.route"
-            >
-              <v-list-item-title>{{ link.name }} </v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-btn>
+      <!--Hamburger icon-->
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
     </v-toolbar>
     <!--side navigation-->
     <v-navigation-drawer class="bg-purple text-white" v-model="drawer">
